f712d2a0607a3191367f506fcbe98f90305e9775
VladislavVik1 <Dakraman1232@ukr.net>
2025-07-31 21:27:40 +0300

    Update Data Base

--- models/Cv.js
+++ models/Cv.js
@@ -6,11 +6,19 @@ const cvSchema = new mongoose.Schema({
   age: String,
   phone: String,
   email: String,
+  github: String,
+  city: String,
+  languages: String,
+  education: String,
+  hobbies: String,
   about: String,
   skills: [String],
+  photo: String,
+  noExperience: Boolean,
+  courseExperience: Boolean,
+  courses: String,
   position: String,
   years: String,
-  noExperience: Boolean,
   createdAt: { type: Date, default: Date.now }
 });
 
